643943714f36d6c288b4e340a046413944b9bd95
belencarlotta <belencarlotta2014@gmail.com>
2023-11-06 22:12:05 -0300

    cambios

--- WebAPI.EM/Controllers/EspecieController.cs
+++ WebAPI.EM/Controllers/EspecieController.cs
@@ -1,4 +1,5 @@
 ﻿using LogicaAplicacion.DTOs;
+using LogicaAplicacion.InterfaceUseCase;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,7 +9,50 @@ namespace WebApiEM.Controllers
     [ApiController]
     public class EspecieController : ControllerBase
     {
-       
+
+        private IWebHostEnvironment _environment;
+        private IAddSpecies AddSpeciesUC;
+        private IGetSpecies GetSpeciesUC;
+        private IGetEcosystem GetEcosystemUC;
+        private IGetEcosystemById GetEcosystemByIdUC;
+        private IAddSpecieToEcosystem AddSpecieToEcosystemUC;
+        private IGetEspeciesPorNombre GetEspeciesPorNombreUC;
+        private IGetPosiblesEcosistemas GetPosiblesEcosistemasUC;
+        private IFiltrado FiltradoUC;
+        private IGetThreats GetThreatsUC;
+        private IGetAmenazaById GetAmenazaByIdUC;
+        private IGetEstadosConservacion GetEstadosConservacionUC;
+        private IUpdateSpecie UpdateSpecieUC;
+        private IAddChangeTracking AddChangeTrackingUC;
+        private IGetSpecieById GetSpecieByIdUC;
+        private IFiltrarDadaUnaEspecie FiltrarDadaUnaEspecieUC;
+
+        public EspecieController(IWebHostEnvironment environment, IAddSpecies addSpeciesUC,
+            IGetSpecies getSpeciesUC, IGetEcosystem getEcosystemUC, IGetEcosystemById getEcosystemByIdUC,
+            IAddSpecieToEcosystem addSpecieToEcosystemUC, IGetEspeciesPorNombre getEspeciesPorNombreUC,
+            IGetPosiblesEcosistemas getPosiblesEcosistemas, IFiltrado filtradoUC, IGetThreats getThreatsUC,
+            IGetAmenazaById getAmenazaByIdUC, IGetEstadosConservacion getEstadosConservacionUC,
+             IUpdateSpecie updateSpecieUC, IAddChangeTracking addChangeTrackingUC, IGetSpecieById getSpecieByIdUC,
+             IFiltrarDadaUnaEspecie filtrarDadaUnaEspecieUC)
+        {
+            _environment = environment;
+            AddSpeciesUC = addSpeciesUC;
+            GetSpeciesUC = getSpeciesUC;
+            GetEcosystemUC = getEcosystemUC;
+            GetEcosystemByIdUC = getEcosystemByIdUC;
+            AddSpecieToEcosystemUC = addSpecieToEcosystemUC;
+            GetEspeciesPorNombreUC = getEspeciesPorNombreUC;
+            GetPosiblesEcosistemasUC = getPosiblesEcosistemas;
+            FiltradoUC = filtradoUC;
+            GetThreatsUC = getThreatsUC;
+            GetAmenazaByIdUC = getAmenazaByIdUC;
+            GetEstadosConservacionUC = getEstadosConservacionUC;
+            UpdateSpecieUC = updateSpecieUC;
+            AddChangeTrackingUC = addChangeTrackingUC;
+            GetSpecieByIdUC = getSpecieByIdUC;
+            FiltrarDadaUnaEspecieUC = filtrarDadaUnaEspecieUC;
+        }
+
         [HttpGet(Name ="GetEcosistemas")]
 
         public IActionResult Get() { 
@@ -20,7 +64,7 @@ namespace WebApiEM.Controllers
         {
             try
             {
-                EspecieDTO especieDTO = this.AddSpeciesUC.AddSpecie(especie);
+                EspecieDTO especieDTO = this.AddSpeciesUC.AddSpecies(especie);
                 return Created("api/Especies", especieDTO);
             }
             catch (Exception ex)
