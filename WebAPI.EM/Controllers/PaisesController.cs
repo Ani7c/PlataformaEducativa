6f3f7161ca4aa128d504e1c0b8b1ef2288587e91
belencarlotta <belencarlotta2014@gmail.com>
2023-11-15 10:32:02 -0300

    Cambios

---
+++ WebAPI.EM/Controllers/PaisesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.EM.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaisesController : ControllerBase
+    {
+        [HttpGet(Name ="GetPaises")]
+
+    }
+}
